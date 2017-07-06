2719cffc07e02b4ae9864b30592b2c33e7d3f5d5
Elton-Santos <cooperton4@gmail.com>
2017-07-06 11:09:54 -0300

    lesson 7

--- connectedin/perfis/urls.py
+++ connectedin/perfis/urls.py
@@ -1,7 +1,10 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+# connectedin/perfis/urls.py
+
+from django.conf.urls import patterns, url
+from perfis import views
 
 urlpatterns = patterns('',
-    url(r'^$', 'perfis.views.index'),
-    url(r'^perfis/(?P<perfil_id>\d+)$', 'perfis.views.exibir'),
+    url(r'^$', views.index, name='index'),
+    url(r'^perfis/(?P<perfil_id>\d+)$', views.exibir, name='exibir'),
+    url(r'^perfis/(?P<perfil_id>\d+)/convidar$', views.convidar, name='convidar'),
 )
